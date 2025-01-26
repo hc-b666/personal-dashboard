d2f91e0d620056d29529ed9b033e6338928a230a
Bobby <bobur0218programmer@gmail.com>
2025-01-26 19:23:42 +0500

    server-fix: fckkkkkkkkkkkk

--- server/dist/config/cors.js
+++ server/dist/config/cors.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsConfig = void 0;
 exports.corsConfig = {
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://personal-dashboard-client.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 };
