bdeb149f11dcfab7b4ea28ea03e7caca7ce41038
Bobby <bobur0218programmer@gmail.com>
2025-01-29 21:00:30 +0500

    feat: dockerized

--- client/src/pages/private/about-me/index.tsx
+++ client/src/pages/private/about-me/index.tsx
@@ -4,7 +4,9 @@ import useAuth from "@/features/auth/hooks/useAuth";
 
 export default function AboutMePage() {
   const { user } = useAuth();
-  const { data, isSuccess } = useFindByUserIdQuery(user?.id as number);
+  const { data, isSuccess } = useFindByUserIdQuery(user?.id as number, {
+    refetchOnMountOrArgChange: true,
+  });
 
   return (
     <div className="col-span-4 my-4 border rounded-md p-10 flex flex-col gap-5">
